ef33f744839dd78954f17df24639e2e98d9e83f3
Rajps44 <singhayush88805@gmail.com>
2024-08-24 00:22:56 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,3 +1,4 @@
+import datetime
 from telethon import Button
 from telethon.events import NewMessage
 from telethon.tl.custom.message import Message
@@ -9,6 +10,27 @@ from bot.modules.decorators import verify_user
 @TelegramBot.on(NewMessage(incoming=True, pattern=r'^/start$'))
 @verify_user(private=True)
 async def welcome(event: NewMessage.Event | Message):
+    user_id = event.sender_id
+    user_data = Telegram.users_collection.find_one({"user_id": user_id})
+
+    # Agar user ka record pehle se hai, to 24 hours check karein
+    if user_data:
+        last_interaction = user_data['first_interaction']
+        current_time = datetime.datetime.now()
+        time_difference = current_time - last_interaction
+
+        # 24 hours (86400 seconds) se zyada ka difference check karte hain
+        if time_difference.total_seconds() > 86400:
+            await event.reply("Aapka 24-hour verification period khatam ho chuka hai. Kripya phir se start kariye.")
+            return
+    else:
+        # Agar user ka record nahi hai, to abhi ka time store karein
+        Telegram.users_collection.insert_one({
+            "user_id": user_id,
+            "first_interaction": datetime.datetime.now()
+        })
+
+    # Agar user authorized hai, to welcome message bheja jaye
     await event.reply(
         message=WelcomeText % {'first_name': event.sender.first_name},
         buttons=[
